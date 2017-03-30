2d83a1a32fa801790059a475c33d6e4c3e6aa973
Sahil <standon196@gmail.com>
2017-03-30 23:52:44 +0530

    collect_main appbar working exactly perfect. it's ui is also done

--- app/src/main/java/com/supreme96/sahil/odk_mocks/MainActivity.java
+++ app/src/main/java/com/supreme96/sahil/odk_mocks/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.blank_form);
+        setContentView(R.layout.collect_main);
     }
 }
